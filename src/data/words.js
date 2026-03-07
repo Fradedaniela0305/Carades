6e9a997ed291a0e7022bc85f37d02d571b22e3b4
trchan <chucan1312@gmail.com>
2026-03-07 15:22:45 -0800

    Starter file for words

---
+++ src/data/words.js
@@ -0,0 +1,104 @@
+export const concepts = [
+
+    {
+        word: "linked list",
+        category: "data structure",
+        hints: "_ _ _ _ _ _   _ _ _ _ "
+    },
+
+    {
+        word: "stack",
+        category: "data structure",
+        hints: "_ _ _ _ _"
+    },
+
+    {
+        word: "queue",
+        category: "data structure",
+        hints: "_ _ _ _ _"
+    },
+
+    {
+        word: "binary tree",
+        category: "data structure",
+        hints: "_ _ _ _ _ _   _ _ _ _"
+    },
+
+    {
+        word: "hash map",
+        category: "data structure",
+        hints: "_ _ _ _   _ _ _"
+    },
+
+    {
+        word: "binary search",
+        category: "algorithm",
+        hints: "_ _ _ _ _ _   _ _ _ _ _ _"
+    },
+
+    {
+        word: "depth-first search",
+        category: "algorithm",
+        hints: "d _ _ _ _-_ _ _ _ _   s _ _ _ _ _"
+    },
+
+    {
+        word: "breadth-first search",
+        category: "algorithm",
+        hints: "b _ _ _ _ _ _-_ _ _ _ _   s _ _ _ _"
+    },
+
+    {
+        word: "quicksort",
+        category: "algorithm",
+        hints: "q _ _ _ _ _ o _ _ "
+    },
+
+    {
+        word: "recursion",
+        category: "programming concept",
+        hints: "r _ _ _ _ s _ _ _"
+    },
+
+    {
+        word: "memoization",
+        category: "programming concept",
+        hints: "m _ _ _ i _ _ _ _ _ _"
+    },
+
+    {
+        word: "polymorphism",
+        category: "programming concept",
+        hints: "p _ _ _ _ _ _ p _ _ _ _"
+    },
+
+    {
+        word: "singleton",
+        category: "design pattern",
+        hints: "s _ _ _ l _ _ _ _"
+    },
+
+    {
+        word: "observer pattern",
+        category: "design pattern",
+        hints: "o _ _ _ _ _ _ _   p _ _ _ _ _ _"
+    },
+
+    {
+        word: "join",
+        category: "database",
+        hints: "j _ _ _"
+    },
+    
+    {
+        word: "overfitting",
+        category: "machine learning",
+        hints: "o _ _ _ _ _ _ t _ _ _"
+    },
+    {
+        word: "underfitting",
+        category: "machine learning",
+        hints: "u _ d _ _ _ _ _ _ _ _ _"
+    }
+
+]
