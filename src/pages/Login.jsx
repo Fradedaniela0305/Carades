806dfb75619205666c768d58274f944e401e7ff7
Daniela Frade <fradenogdaniela@gmail.com>
2026-03-07 14:20:21 -0800

    added login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+export function Login() {
+
+
+
+
+
+
+
+    return (
+        <>
+        <div> 
+            <h4>nickname</h4>
+            <input/>
+            <h4>languages</h4>
+            <input/>
+        </div>
+        </>
+    )
+
+}
