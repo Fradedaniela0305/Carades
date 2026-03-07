377c0b5e5d99552d2fe4ff42b9d514c84c29468e
trchan <chucan1312@gmail.com>
2026-03-07 14:14:34 -0800

    Add home debug page for navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Game from './pages/Game.jsx'
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Game from "./pages/Game"
+
+import Leaderboard from "./components/Leaderboard"
+import GuessChat from "./components/GuessChat"
+import CodeEditor from "./components/CodeEditor"
+import PopUp from "./components/PopUp"
 
 function App() {
-    const [count, setCount] = useState(0)
 
-    return (
-        <>
-            <Game />
-        </>
-    )
+  return (
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/game" element={<Game />} />
+
+      <Route path="/leaderboard" element={<Leaderboard />} />
+
+      <Route path="/chat" element={<GuessChat />} />
+
+      <Route path="/editor" element={<CodeEditor />} />
+
+      <Route path="/popup" element={<PopUp />} />
+
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+
+    return (
+        <div style={{ padding: "40px" }}>
+
+            <h1>Dev Navigation</h1>
+            <p>Use this page to test components</p>
+
+            <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+
+                <Link to="/login">
+                    <button>Login Page</button>
+                </Link>
+
+                <Link to="/game">
+                    <button>Game Page</button>
+                </Link>
+
+                <Link to="/leaderboard">
+                    <button>Leaderboard</button>
+                </Link>
+
+                <Link to="/chat">
+                    <button>Guess Chat</button>
+                </Link>
+
+                <Link to="/editor">
+                    <button>Code Editor</button>
+                </Link>
+
+                <Link to="/popup">
+                    <button>Pop Up</button>
+                </Link>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
