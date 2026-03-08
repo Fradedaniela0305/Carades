4c492fb677a59d7f738b025a7cb1745f689490cd
Joy Li <joyxinran@gmail.com>
2026-03-08 10:04:04 -0700

    more wordssss

--- src/data/words.js
+++ src/data/words.js
@@ -1,245 +1,341 @@
 export const concepts = [
-    {
-        word: "linked list",
-        category: "data structure",
-        hints: "l _ _ _ e _      l _ _ _ ",
-    },
+  {
+    word: "linked list",
+    category: "data structure",
+    hints: "l _ _ _ e _      l _ _ _ ",
+  },
 
-    {
-        word: "stack",
-        category: "data structure",
-        hints: "s _ _ _ _",
-    },
+  {
+    word: "stack",
+    category: "data structure",
+    hints: "s _ _ _ _",
+  },
 
-    {
-        word: "queue",
-        category: "data structure",
-        hints: "q _ _ _ _",
-    },
+  {
+    word: "queue",
+    category: "data structure",
+    hints: "q _ _ _ _",
+  },
 
-    {
-        word: "binary tree",
-        category: "data structure",
-        hints: "b _ _ _ _ _      t _ _ _",
-    },
+  {
+    word: "binary tree",
+    category: "data structure",
+    hints: "b _ _ _ _ _      t _ _ _",
+  },
 
-    {
-        word: "hash map",
-        category: "data structure",
-        hints: "h _ _ _      m _ _",
-    },
+  {
+    word: "hash map",
+    category: "data structure",
+    hints: "h _ _ _      m _ _",
+  },
 
-    {
-        word: "binary search",
-        category: "algorithm",
-        hints: "b _ _ _ _ _      s _ a _ _ _",
-    },
+  {
+    word: "binary search",
+    category: "algorithm",
+    hints: "b _ _ _ _ _      s _ a _ _ _",
+  },
 
-    {
-        word: "depth-first search",
-        category: "algorithm",
-        hints: "d _ _ _ _-_ _ _ _ _      s _ _ _ _ _",
-    },
+  {
+    word: "depth-first search",
+    category: "algorithm",
+    hints: "d _ _ _ _-_ _ _ _ _      s _ _ _ _ _",
+  },
 
-    {
-        word: "breadth-first search",
-        category: "algorithm",
-        hints: "b _ _ _ _ _ _-_ _ _ _ _      s _ _ _ _",
-    },
+  {
+    word: "breadth-first search",
+    category: "algorithm",
+    hints: "b _ _ _ _ _ _-_ _ _ _ _      s _ _ _ _",
+  },
 
-    {
-        word: "quicksort",
-        category: "algorithm",
-        hints: "q _ _ _ _ _ o _ _ ",
-    },
+  {
+    word: "recursion",
+    category: "programming concept",
+    hints: "r _ _ _ _ s _ _ _",
+  },
 
-    {
-        word: "recursion",
-        category: "programming concept",
-        hints: "r _ _ _ _ s _ _ _",
-    },
+  {
+    word: "memoization",
+    category: "programming concept",
+    hints: "m _ _ _ i _ _ _ _ _ _",
+  },
 
-    {
-        word: "memoization",
-        category: "programming concept",
-        hints: "m _ _ _ i _ _ _ _ _ _",
-    },
+  {
+    word: "polymorphism",
+    category: "programming concept",
+    hints: "p _ _ _ _ _ _ p _ _ _ _",
+  },
 
-    {
-        word: "polymorphism",
-        category: "programming concept",
-        hints: "p _ _ _ _ _ _ p _ _ _ _",
-    },
+  {
+    word: "singleton",
+    category: "design pattern",
+    hints: "s _ _ _ l _ _ _ _",
+  },
 
-    {
-        word: "singleton",
-        category: "design pattern",
-        hints: "s _ _ _ l _ _ _ _",
-    },
+  {
+    word: "observer pattern",
+    category: "design pattern",
+    hints: "o _ _ _ _ v _ _      p _ _ _ _ _ _",
+  },
 
-    {
-        word: "observer pattern",
-        category: "design pattern",
-        hints: "o _ _ _ _ v _ _      p _ _ _ _ _ _",
-    },
+  {
+    word: "join",
+    category: "database",
+    hints: "j _ _ _",
+  },
 
-    {
-        word: "join",
-        category: "database",
-        hints: "j _ _ _",
-    },
-
-    {
-        word: "overfitting",
-        category: "machine learning",
-        hints: "o _ _ _ _ _ _ t _ _ _",
-    },
-    {
-        word: "underfitting",
-        category: "machine learning",
-        hints: "u _ d _ _ _ _ _ _ _ _ _",
-    },
-    {
-        word: "bubble sort",
-        category: "algorithm",
-        hints: "b _ _ _ _ e      s _ _ _",
-    },
-    {
-        word: "fibonacci",
-        category: "algorithm",
-        hints: "f _ _ _ _ _ _ _ i",
-    },
-    {
-        word: "fizzbuzz",
-        category: "algorithm",
-        hints: "f _ _ _ b _ _ _",
-    },
-    {
-        word: "inheritance",
-        category: "programming concept",
-        hints: "i _ _ _ _ _ _ _ _ _ e",
-    },
-    {
-        word: "encapsulation",
-        category: "programming concept",
-        hints: "e _ _ _ _ _ _ _ _ _ _ _ n",
-    },
-    {
-        word: "async",
-        category: "programming concept",
-        hints: "a _ _ _ _",
-    },
-    {
-        word: "factorial",
-        category: "mathematics",
-        hints: "f _ _ _ _ _ _ _ l",
-    },
-    {
-        word: "bitwise operator",
-        category: "programming concept",
-        hints: "b _ _ _ _ _ e      o _ _ _ _ _ _ r",
-    },
-    {
-        word: "prime numbers",
-        category: "mathematics",
-        hints: "p _ _ _ e      n _ _ _ _ _ s",
-    },
-    {
-        word: "dijkstra",
-        category: "algorithm",
-        hints: "d _ _ _ _ _ _ a",
-    },
-    {
-        word: "a star",
-        category: "algorithm",
-        hints: "a     s _ _ r",
-    },
-    {
-        word: "flood fill",
-        category: "algorithm",
-        hints: "f _ _ _ d      f _ _ _",
-    },
-    {
-        word: "merge sort",
-        category: "algorithm",
-        hints: "m _ _ _ e      s _ _ _",
-    },
-    {
-        word: "quicksort",
-        category: "algorithm",
-        hints: "q _ _ _ _ _ _ _ t",
-    },
-    {
-        word: "insertion sort",
-        category: "algorithm",
-        hints: "i _ _ _ _ _ _ _ n      s _ _ _",
-    },
-    {
-        word: "for loop",
-        category: "programming concept",
-        hints: "f _ _      l _ _ _"
-    },
-    {
-        word: "if statement",
-        category: "programming concept",
-        hints: "i _      s _ _ _ _ _ e _ _"
-    },
-    {
-        word: "while loop",
-        category: "programming concept",
-        hints: "w _ _ _ _      l _ _ _"
-    },
-    {
-        word: "ddl",
-        category: "database",
-        hints: "d _ _"
-    },
-    {
-        word: "primary key",
-        category: "database",
-        hints: "p _ _ _ _ _ _      k _ _"
-    },{
-        word: "boolean",
-        category: "programming concept",
-        hints: "b _ _ _ _ _ _"
-    }, {
-        word: "api",
-        category: "software concept",
-        hints: "a _ _"
-    },
-    {
-        word: "syntax error",
-        category: "bug",
-        hints: "s _ _ _ _ _      e _ _ _ _"
-    },
-    {
-        word: "memory leak",
-        category: "bug",
-        hints: "m _ _ _ _ y      l _ _ _"
-    },
-    {
-        word: "dangling pointer",
-        category: "bug",
-        hints: "d _ _ _ _ i _ _      p _ _ _ _ _ r"
-    },
-    {
-        word: "hello world",
-        category: "programming culture",
-        hints: "h _ _ _ _      w _ _ _ _"
-    }, 
-    {
-        word: "not found",
-        category: "response status",
-        hints: "n _ _      f _ _ _ _"
-    }, 
-    {
-        word: "ok",
-        category: "response status",
-        hints: "o _"
-    },
-    {
-        word: "bad request",
-        category: "response status",
-        hints: "b _ _      r _ _ _ _ _ _"
-    }
+  {
+    word: "overfitting",
+    category: "machine learning",
+    hints: "o _ _ _ _ _ _ t _ _ _",
+  },
+  {
+    word: "underfitting",
+    category: "machine learning",
+    hints: "u _ d _ _ _ _ _ _ _ _ _",
+  },
+  {
+    word: "bubble sort",
+    category: "algorithm",
+    hints: "b _ _ _ _ e      s _ _ _",
+  },
+  {
+    word: "fibonacci",
+    category: "algorithm",
+    hints: "f _ _ _ _ _ _ _ i",
+  },
+  {
+    word: "fizzbuzz",
+    category: "algorithm",
+    hints: "f _ _ _ b _ _ _",
+  },
+  {
+    word: "inheritance",
+    category: "programming concept",
+    hints: "i _ _ _ _ _ _ _ _ _ e",
+  },
+  {
+    word: "encapsulation",
+    category: "programming concept",
+    hints: "e _ _ _ _ _ _ _ _ _ _ _ n",
+  },
+  {
+    word: "async",
+    category: "programming concept",
+    hints: "a _ _ _ _",
+  },
+  {
+    word: "factorial",
+    category: "mathematics",
+    hints: "f _ _ _ _ _ _ _ l",
+  },
+  {
+    word: "bitwise operator",
+    category: "programming concept",
+    hints: "b _ _ _ _ _ e      o _ _ _ _ _ _ r",
+  },
+  {
+    word: "prime numbers",
+    category: "mathematics",
+    hints: "p _ _ _ e      n _ _ _ _ _ s",
+  },
+  {
+    word: "dijkstra",
+    category: "algorithm",
+    hints: "d _ _ _ _ _ _ a",
+  },
+  {
+    word: "a star",
+    category: "algorithm",
+    hints: "a     s _ _ r",
+  },
+  {
+    word: "flood fill",
+    category: "algorithm",
+    hints: "f _ _ _ d      f _ _ _",
+  },
+  {
+    word: "merge sort",
+    category: "algorithm",
+    hints: "m _ _ _ e      s _ _ _",
+  },
+  {
+    word: "quicksort",
+    category: "algorithm",
+    hints: "q _ _ _ _ _ _ _ t",
+  },
+  {
+    word: "insertion sort",
+    category: "algorithm",
+    hints: "i _ _ _ _ _ _ _ n      s _ _ _",
+  },
+  {
+    word: "for loop",
+    category: "programming concept",
+    hints: "f _ _      l _ _ _",
+  },
+  {
+    word: "if statement",
+    category: "programming concept",
+    hints: "i _      s _ _ _ _ _ e _ _",
+  },
+  {
+    word: "while loop",
+    category: "programming concept",
+    hints: "w _ _ _ _      l _ _ _",
+  },
+  {
+    word: "ddl",
+    category: "database",
+    hints: "d _ _",
+  },
+  {
+    word: "primary key",
+    category: "database",
+    hints: "p _ _ _ _ _ _      k _ _",
+  },
+  {
+    word: "boolean",
+    category: "programming concept",
+    hints: "b _ _ _ _ _ _",
+  },
+  {
+    word: "api",
+    category: "software concept",
+    hints: "a _ _",
+  },
+  {
+    word: "syntax error",
+    category: "bug",
+    hints: "s _ _ _ _ _      e _ _ _ _",
+  },
+  {
+    word: "memory leak",
+    category: "bug",
+    hints: "m _ _ _ _ y      l _ _ _",
+  },
+  {
+    word: "dangling pointer",
+    category: "bug",
+    hints: "d _ _ _ _ i _ _      p _ _ _ _ _ r",
+  },
+  {
+    word: "hello world",
+    category: "programming culture",
+    hints: "h _ _ _ _      w _ _ _ _",
+  },
+  {
+    word: "not found",
+    category: "response status",
+    hints: "n _ _      f _ _ _ _",
+  },
+  {
+    word: "ok",
+    category: "response status",
+    hints: "o _",
+  },
+  {
+    word: "bad request",
+    category: "response status",
+    hints: "b _ _      r _ _ _ _ _ _",
+  },
+  {
+    word: "triangle pattern",
+    category: "pattern",
+    hints: "t _ _ _ _ _ _      p _ _ _ _ _ _",
+  },
+  {
+    word: "square pattern",
+    category: "pattern",
+    hints: "s _ _ _ _ _      p _ _ _ _ _ _",
+  },
+  {
+    word: "even numbers",
+    category: "mathematics",
+    hints: "e _ _ n      n _ _ _ _ _ s",
+  },
+  {
+    word: "odd numbers",
+    category: "mathematics",
+    hints: "o _ _      n _ _ _ _ _ s",
+  },
+  {
+    word: "countdown",
+    category: "algorithm",
+    hints: "c _ _ _ _ _ _ _ _",
+  },
+  {
+    word: "palindrome",
+    category: "string problem",
+    hints: "p _ _ _ _ _ _ _ _ e",
+  },
+  {
+    word: "chessboard pattern",
+    category: "pattern",
+    hints: "c _ _ _ _ _ _ _ _      p _ _ _ _ _ _",
+  },
+  {
+    word: "checkerboard",
+    category: "pattern",
+    hints: "c _ _ _ _ _ _ _ _ _ _",
+  },
+  {
+    word: "multiplication grid",
+    category: "pattern",
+    hints: "m _ _ _ _ _ _ _ _ _ _ _ _ n      g _ _ _",
+  },
+  {
+    word: "x pattern",
+    category: "pattern",
+    hints: "x      p _ _ _ _ _ _",
+  },
+  {
+    word: "dice roll",
+    category: "simulation",
+    hints: "d _ _ e      r _ _ _",
+  },
+  {
+    word: "coin flip",
+    category: "simulation",
+    hints: "c _ _ _      f _ _ _",
+  },
+  {
+    word: "maximum",
+    category: "algorithm",
+    hints: "m _ _ _ _ _ _",
+  },
+  {
+    word: "minimum",
+    category: "algorithm",
+    hints: "m _ _ _ _ _ _",
+  },
+  {
+    word: "average",
+    category: "algorithm",
+    hints: "a _ _ _ _ _ e",
+  },
+  {
+    word: "reverse list",
+    category: "array problem",
+    hints: "r _ _ _ _ _ e      l _ _ _",
+  },
+  {
+    word: "count occurrences",
+    category: "array problem",
+    hints: "c _ _ _ _      o _ _ _ _ _ _ _ _ _ _",
+  },
+  {
+    word: "second largest number",
+    category: "array problem",
+    hints: "s _ _ _ _ _      l _ _ _ _ _ _      n _ _ _ _ _",
+  },
+  {
+    word: "merge two lists",
+    category: "array problem",
+    hints: "m _ _ _ e      t _ _      l _ _ _ _",
+  },
+  {
+    word: "powers of two",
+    category: "mathematics",
+    hints: "p _ _ _ _ s      o _      t _ _",
+  },
 ];
