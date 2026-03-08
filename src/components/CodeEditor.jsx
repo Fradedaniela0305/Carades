c849f0f1cf89e41459fa238e9c25464848ff9550
Joy Li <joyxinran@gmail.com>
2026-03-07 21:06:06 -0800

    spectator to guesser

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -75,7 +75,7 @@ export default function CodeEditor({ roomID, isCoder }) {
           <>
             <div className="h-2 w-2 rounded-full bg-slate-500 shadow-[0_0_4px_rgba(255,255,255,0.2)]" />
             <span className="font-goldman text-xs tracking-[0.2em] text-slate-400 uppercase italic font-bold">
-              Role: Spectator
+              Role: Guesser
             </span>
           </>
         )}
