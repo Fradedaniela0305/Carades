50ea38377373acfa35acf615c3b63e22fc98caf5
trchan <chucan1312@gmail.com>
2026-03-08 00:06:17 -0800

    Add words

--- src/data/words.js
+++ src/data/words.js
@@ -1,182 +1,202 @@
 export const concepts = [
-  {
-    word: "linked list",
-    category: "data structure",
-    hints: "_ _ _ _ _ _   _ _ _ _ ",
-  },
+    {
+        word: "linked list",
+        category: "data structure",
+        hints: "l _ _ _ e _      l _ _ _ ",
+    },
 
-  {
-    word: "stack",
-    category: "data structure",
-    hints: "_ _ _ _ _",
-  },
+    {
+        word: "stack",
+        category: "data structure",
+        hints: "s _ _ _ _",
+    },
 
-  {
-    word: "queue",
-    category: "data structure",
-    hints: "_ _ _ _ _",
-  },
+    {
+        word: "queue",
+        category: "data structure",
+        hints: "q _ _ _ _",
+    },
 
-  {
-    word: "binary tree",
-    category: "data structure",
-    hints: "_ _ _ _ _ _   _ _ _ _",
-  },
+    {
+        word: "binary tree",
+        category: "data structure",
+        hints: "b _ _ _ _ _      t _ _ _",
+    },
 
-  {
-    word: "hash map",
-    category: "data structure",
-    hints: "_ _ _ _   _ _ _",
-  },
+    {
+        word: "hash map",
+        category: "data structure",
+        hints: "h _ _ _      m _ _",
+    },
 
-  {
-    word: "binary search",
-    category: "algorithm",
-    hints: "_ _ _ _ _ _   _ _ _ _ _ _",
-  },
+    {
+        word: "binary search",
+        category: "algorithm",
+        hints: "b _ _ _ _ _      s _ a _ _ _",
+    },
 
-  {
-    word: "depth-first search",
-    category: "algorithm",
-    hints: "d _ _ _ _-_ _ _ _ _   s _ _ _ _ _",
-  },
+    {
+        word: "depth-first search",
+        category: "algorithm",
+        hints: "d _ _ _ _-_ _ _ _ _      s _ _ _ _ _",
+    },
 
-  {
-    word: "breadth-first search",
-    category: "algorithm",
-    hints: "b _ _ _ _ _ _-_ _ _ _ _   s _ _ _ _",
-  },
+    {
+        word: "breadth-first search",
+        category: "algorithm",
+        hints: "b _ _ _ _ _ _-_ _ _ _ _      s _ _ _ _",
+    },
 
-  {
-    word: "quicksort",
-    category: "algorithm",
-    hints: "q _ _ _ _ _ o _ _ ",
-  },
+    {
+        word: "quicksort",
+        category: "algorithm",
+        hints: "q _ _ _ _ _ o _ _ ",
+    },
 
-  {
-    word: "recursion",
-    category: "programming concept",
-    hints: "r _ _ _ _ s _ _ _",
-  },
+    {
+        word: "recursion",
+        category: "programming concept",
+        hints: "r _ _ _ _ s _ _ _",
+    },
 
-  {
-    word: "memoization",
-    category: "programming concept",
-    hints: "m _ _ _ i _ _ _ _ _ _",
-  },
+    {
+        word: "memoization",
+        category: "programming concept",
+        hints: "m _ _ _ i _ _ _ _ _ _",
+    },
 
-  {
-    word: "polymorphism",
-    category: "programming concept",
-    hints: "p _ _ _ _ _ _ p _ _ _ _",
-  },
+    {
+        word: "polymorphism",
+        category: "programming concept",
+        hints: "p _ _ _ _ _ _ p _ _ _ _",
+    },
 
-  {
-    word: "singleton",
-    category: "design pattern",
-    hints: "s _ _ _ l _ _ _ _",
-  },
+    {
+        word: "singleton",
+        category: "design pattern",
+        hints: "s _ _ _ l _ _ _ _",
+    },
 
-  {
-    word: "observer pattern",
-    category: "design pattern",
-    hints: "o _ _ _ _ _ _ _   p _ _ _ _ _ _",
-  },
+    {
+        word: "observer pattern",
+        category: "design pattern",
+        hints: "o _ _ _ _ v _ _      p _ _ _ _ _ _",
+    },
 
-  {
-    word: "join",
-    category: "database",
-    hints: "j _ _ _",
-  },
+    {
+        word: "join",
+        category: "database",
+        hints: "j _ _ _",
+    },
 
-  {
-    word: "overfitting",
-    category: "machine learning",
-    hints: "o _ _ _ _ _ _ t _ _ _",
-  },
-  {
-    word: "underfitting",
-    category: "machine learning",
-    hints: "u _ d _ _ _ _ _ _ _ _ _",
-  },
-  {
-    word: "bubble sort",
-    category: "algorithm",
-    hints: "b _ _ _ _ e   s _ _ _",
-  },
-  {
-    word: "fibonacci",
-    category: "algorithm",
-    hints: "f _ _ _ _ _ _ _ i",
-  },
-  {
-    word: "fizzbuzz",
-    category: "algorithm",
-    hints: "f _ _ _ b _ _ _",
-  },
-  {
-    word: "inheritance",
-    category: "programming concept",
-    hints: "i _ _ _ _ _ _ _ _ _ e",
-  },
-  {
-    word: "encapsulation",
-    category: "programming concept",
-    hints: "e _ _ _ _ _ _ _ _ _ _ _ n",
-  },
-  {
-    word: "async await",
-    category: "programming concept",
-    hints: "a _ _ _ _   a _ _ _ _",
-  },
-  {
-    word: "factorial",
-    category: "mathematics",
-    hints: "f _ _ _ _ _ _ _ l",
-  },
-  {
-    word: "bitwise operator",
-    category: "programming concept",
-    hints: "b _ _ _ _ _ e   o _ _ _ _ _ _ r",
-  },
-  {
-    word: "prime numbers",
-    category: "algorithm",
-    hints: "p _ _ _ e   n _ _ _ _ _ s",
-  },
-  {
-    word: "gcd",
-    category: "algorithm",
-    hints: "g _ d",
-  },
-  {
-    word: "dijkstra",
-    category: "algorithm",
-    hints: "d _ _ _ _ _ _ a",
-  },
-  {
-    word: "a star",
-    category: "algorithm",
-    hints: "a _ s _ _ r",
-  },
-  {
-    word: "flood fill",
-    category: "algorithm",
-    hints: "f _ _ _ d   f _ _ l",
-  },
-  {
-    word: "merge sort",
-    category: "algorithm",
-    hints: "m _ _ _ e   s _ _ _",
-  },
-  {
-    word: "quicksort",
-    category: "algorithm",
-    hints: "q _ _ _ _ _ _ _ t",
-  },
-  {
-    word: "insertion sort",
-    category: "algorithm",
-    hints: "i _ _ _ _ _ _ _ n   s _ _ _",
-  },
+    {
+        word: "overfitting",
+        category: "machine learning",
+        hints: "o _ _ _ _ _ _ t _ _ _",
+    },
+    {
+        word: "underfitting",
+        category: "machine learning",
+        hints: "u _ d _ _ _ _ _ _ _ _ _",
+    },
+    {
+        word: "bubble sort",
+        category: "algorithm",
+        hints: "b _ _ _ _ e      s _ _ _",
+    },
+    {
+        word: "fibonacci",
+        category: "algorithm",
+        hints: "f _ _ _ _ _ _ _ i",
+    },
+    {
+        word: "fizzbuzz",
+        category: "algorithm",
+        hints: "f _ _ _ b _ _ _",
+    },
+    {
+        word: "inheritance",
+        category: "programming concept",
+        hints: "i _ _ _ _ _ _ _ _ _ e",
+    },
+    {
+        word: "encapsulation",
+        category: "programming concept",
+        hints: "e _ _ _ _ _ _ _ _ _ _ _ n",
+    },
+    {
+        word: "async",
+        category: "programming concept",
+        hints: "a _ _ _ _",
+    },
+    {
+        word: "factorial",
+        category: "mathematics",
+        hints: "f _ _ _ _ _ _ _ l",
+    },
+    {
+        word: "bitwise operator",
+        category: "programming concept",
+        hints: "b _ _ _ _ _ e      o _ _ _ _ _ _ r",
+    },
+    {
+        word: "prime numbers",
+        category: "algorithm",
+        hints: "p _ _ _ e      n _ _ _ _ _ s",
+    },
+    {
+        word: "dijkstra",
+        category: "algorithm",
+        hints: "d _ _ _ _ _ _ a",
+    },
+    {
+        word: "a star",
+        category: "algorithm",
+        hints: "a     s _ _ r",
+    },
+    {
+        word: "flood fill",
+        category: "algorithm",
+        hints: "f _ _ _ d      f _ _ _",
+    },
+    {
+        word: "merge sort",
+        category: "algorithm",
+        hints: "m _ _ _ e      s _ _ _",
+    },
+    {
+        word: "quicksort",
+        category: "algorithm",
+        hints: "q _ _ _ _ _ _ _ t",
+    },
+    {
+        word: "insertion sort",
+        category: "algorithm",
+        hints: "i _ _ _ _ _ _ _ n      s _ _ _",
+    },
+    {
+        word: "for loop",
+        category: "programming concept",
+        hints: "f _ _      l _ _ _"
+    },
+    {
+        word: "if statement",
+        category: "programming concept",
+        hints: "i _      s _ _ _ _ _ e _ _"
+    },
+    {
+        word: "while loop",
+        category: "programming concept",
+        hints: "w _ _ _ _      l _ _ _"
+    },
+    {
+        word: "ddl",
+        category: "database",
+        hints: "d _ _"
+    },
+    {
+        word: "primary key",
+        category: "database",
+        hints: "p _ _ _ _ _ _      k _ _"
+    }
 ];
